b55929413f568a61b2995b3b60983fdcf0cf87e2
AdisBrkic <adistfb@hotmail.com>
2019-04-13 01:03:59 +0200

    Add run ui tests file

---
+++ run-ui-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+export VERSION=`jq .[0].release SHOPVERSIONS`
+
+curl -s https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > ngrok.zip
+unzip ngrok.zip
+chmod +x $PWD/ngrok
+
+curl -sO http://stedolan.github.io/jq/download/linux64/jq
+chmod +x $PWD/jq
+
+$PWD/ngrok authtoken 53c6WVprQB4dpNDZnLiLr_2XALf7e5gUCFALtrz5pmE
+TIMESTAMP=$(date +%s)
+$PWD/ngrok http 9090 -subdomain="${TIMESTAMP}-magento2-api-test" > /dev/null &
+
+NGROK_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
+
+while [ ! ${NGROK_URL} ] || [ ${NGROK_URL} = 'null' ];  do
+    echo "Waiting for ngrok to initialize"
+    export NGROK_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
+    echo $NGROK_URL
+    sleep 1
+done
+
+bash .bin/start-shopsystem.sh
