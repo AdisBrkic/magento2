874816cec3aea82d506276f4f7fe33002fce619f
adisbr <adis_brkic@hotmail.com>
2019-04-13 00:14:03 +0200

    Add to travis

---
+++ start-shopsystem.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+export MAGENTO_CONTAINER_NAME=web
+
+docker-compose build --build-arg GATEWAY=API-TEST web
+docker-compose up > /dev/null &
+
+while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
+    echo "Waiting for docker container to initialize"
+    sleep 5
+done
+
+docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
+docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:static-content:deploy en_US -f
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:flush
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento indexer:reindex
+sleep 60s
+docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
+
+docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./"
