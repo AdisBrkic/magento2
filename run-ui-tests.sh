9910deefce1d9ea590de64522ab05d82aee8ea4d
AdisBrkic <adistfb@hotmail.com>
2019-04-14 19:31:45 +0200

    Install shop

--- run-ui-tests.sh
+++ run-ui-tests.sh
@@ -3,9 +3,24 @@ set -e
 
 export VERSION=`jq .[0].release SHOPVERSIONS`
 
-git clone https://github.com/AdisBrkic/docker-magento2.git
-cd docker-magento2
-docker-compose up -d
-docker exec -it dockermagento2_web_1 install-magento
-docker exec -it dockermagento2_web_1 install-sampledata
-sleep 30s
+curl -s https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > ngrok.zip
+unzip ngrok.zip
+chmod +x $PWD/ngrok
+
+curl -sO http://stedolan.github.io/jq/download/linux64/jq
+chmod +x $PWD/jq
+
+$PWD/ngrok authtoken 53c6WVprQB4dpNDZnLiLr_2XALf7e5gUCFALtrz5pmE
+TIMESTAMP=$(date +%s)
+$PWD/ngrok http 9090 -subdomain="${TIMESTAMP}-magento2-api-test" > /dev/null &
+
+NGROK_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
+
+while [ ! ${NGROK_URL} ] || [ ${NGROK_URL} = 'null' ];  do
+    echo "Waiting for ngrok to initialize"
+    export NGROK_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
+    echo $NGROK_URL
+    sleep 20
+done
+
+bash start-shopsystem.sh
