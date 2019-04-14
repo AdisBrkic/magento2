8a73c8f6b5b6d71a51bb908bf9b52c0b17aa7a36
AdisBrkic <adistfb@hotmail.com>
2019-04-14 18:26:49 +0200

    Install magento2

--- run-ui-tests.sh
+++ run-ui-tests.sh
@@ -6,6 +6,6 @@ export VERSION=`jq .[0].release SHOPVERSIONS`
 git clone https://github.com/AdisBrkic/docker-magento2.git
 cd docker-magento2
 docker-compose up -d
-docker exec -it docker-magento2_web_1 install-magento
-docker exec -it docker-magento2_web_1 install-sampledata
+docker exec -it dockermagento2_web_1 install-magento
+docker exec -it dockermagento2_web_1 install-sampledata
 sleep 2m
