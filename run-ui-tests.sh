acf6b68a24f13f487b822ce337d9d5f16e207c7a
AdisBrkic <adistfb@hotmail.com>
2019-04-14 22:49:42 +0200

    Create ngrok

--- run-ui-tests.sh
+++ run-ui-tests.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 set -e
 
-export VERSION=`jq .[0].release SHOPVERSIONS`
-
 curl -s https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > ngrok.zip
 unzip ngrok.zip
-chmod +x $PWD/ngrok
+chmod +x ngrok
 
-curl -sO http://stedolan.github.io/jq/download/linux64/jq
-chmod +x $PWD/jq
+ngrok authtoken 53c6WVprQB4dpNDZnLiLr_2XALf7e5gUCFALtrz5pmE
 
-$PWD/ngrok authtoken 53c6WVprQB4dpNDZnLiLr_2XALf7e5gUCFALtrz5pmE
-TIMESTAMP=$(date +%s)
-$PWD/ngrok http 9090 -subdomain="${TIMESTAMP}-magento2-api-test" > /dev/null &
-
-NGROK_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
+NGROK_URL = ngrok http 9090 -subdomain="magento2-api-test" > /dev/null &
 
 while [ ! ${NGROK_URL} ] || [ ${NGROK_URL} = 'null' ];  do
     echo "Waiting for ngrok to initialize"
-    export NGROK_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
+    export NGROK_URL
     echo $NGROK_URL
-    sleep 20
+    sleep 5
 done
 
 bash start-shopsystem.sh
