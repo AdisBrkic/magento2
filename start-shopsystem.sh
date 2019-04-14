66a5145134354710fc7967cb49d177cd4cbf6ebb
AdisBrkic <adistfb@hotmail.com>
2019-04-14 22:31:57 +0200

    Remove volumes and add docker stop and remove

--- start-shopsystem.sh
+++ start-shopsystem.sh
@@ -3,6 +3,8 @@ set -e
 
 export MAGENTO_CONTAINER_NAME=web
 
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 docker-compose build --build-arg GATEWAY=API-TEST web
 docker-compose up > /dev/null &
 
@@ -13,15 +15,5 @@ done
 
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:static-content:deploy -f de_DE
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento indexer:reindex
-#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./"
 echo "Deploy finished!"
-sleep 120s
-#docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
-#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
-#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
-
-#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./"
+sleep 240s
