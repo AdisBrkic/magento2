f5d5545a06d4d91f02dfc8f8a30f5c707762a5b4
AdisBrkic <adistfb@hotmail.com>
2019-04-14 12:35:47 +0200

    Install magento2 shop

--- start-shopsystem.sh
+++ start-shopsystem.sh
@@ -8,19 +8,19 @@ docker-compose up > /dev/null &
 
 while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
     echo "Waiting for docker container to initialize"
-    sleep 5
+    sleep 1
 done
 
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:clean
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:flush
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:clean
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:flush
 echo "Flush zavrsen"
 
 sleep 90s
-docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
+#docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
 
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./"
+#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./"
