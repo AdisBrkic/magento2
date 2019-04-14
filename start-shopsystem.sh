a49897872737e3f10375d3ef8f1676a45e541543
AdisBrkic <adistfb@hotmail.com>
2019-04-14 23:17:41 +0200

    Create ngrok and show containers

--- start-shopsystem.sh
+++ start-shopsystem.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-export MAGENTO_CONTAINER_NAME=web
-
 docker-compose build --build-arg GATEWAY=API-TEST web
 docker-compose up > /dev/null &
-docker ps -a
 
-while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
+while ! $(curl --output /dev/null --silent --head --fail http://f02fc6d3.ngrok.io); do
     echo "Waiting for docker container to initialize"
     sleep 1
 done
 
-docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
-docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
+docker exec -it web install-magento
+docker exec -it web install-sampledata
 echo "Deploy finished!"
 sleep 240s
